701ffbd49cd5336e8e31ed0a1a3040a86b8db5d3
masayoshikuroda <kuroda.masayoshi@gmail.com>
2021-02-21 22:13:16 +0900

    Introduced SwitchBot class 	modified:   switchbot.py 	meter.py

--- switchbot.py
+++ switchbot.py
@@ -8,57 +8,71 @@ from urllib.parse import urlencode
 from urllib.request import urlopen, Request, URLError, HTTPError
 from argparse import ArgumentParser
 
-BASE_URL        = "https://api.switch-bot.com"
-BASE_PATH       = os.path.dirname(os.path.abspath(__file__))
-TOKEN_FILE      = os.path.join(BASE_PATH, 'token.txt')
-DEVICES_FILE    = os.path.join(BASE_PATH, 'devices.json')
+class SwitchBot:
+    BASE_URL        = 'https://api.switch-bot.com'
+    BASE_PATH       = os.path.dirname(os.path.abspath(__file__))
+    TOKEN_FILE      = os.path.join(BASE_PATH, 'token.txt')
+    DEVICES_FILE    = os.path.join(BASE_PATH, 'devices.json')
 
-usage = 'Usage: python {} command [--dev_id dev_id]'.format(__file__)
-commands = '|'.join(['get_devices', 'get_status', 'post_action'])
-argparser = ArgumentParser(usage=usage)
-argparser.add_argument('command', type=str, help=commands)
-argparser.add_argument('-d', '--dev_id',    type=str, dest='dev_id',  default='dev_id',  help='device id for reading status')
-argparser.add_argument('-a', '--action',    type=str, dest='action',  default='turnOn',  help='command to send device')
-argparser.add_argument('-p', '--parameter', type=str, dest='param',   default='default', help='parameter of command')
-argparser.add_argument('-t', '--type',      type=str, dest='type',    default='command', help='type of command')
-args = argparser.parse_args()
+    def __init__(self):
+        with open(SwitchBot.TOKEN_FILE, 'r') as f:
+            self.token = f.read().strip()
 
-def get_token():
-    with open(TOKEN_FILE, 'r') as f:
-        token = f.read().strip()
-        # print(token)
-        return token
+    def get_headers(self):
+        return { 'Authorization': self.token }
 
-token = get_token()
+    def get_url(self, *pathes):
+        url = SwitchBot.BASE_URL + '/v1.0'
+        for path in pathes:
+            url += '/' + path
+        return url
 
-url = BASE_URL + '/v1.0'
-headers = { 'Authorization': token } 
-data = None
-if args.command.startswith('get_dev'):
-    url += '/devices'
-elif args.command.startswith('get_sta'):
-    url += '/devices/' + args.dev_id + '/status'
-elif args.command.startswith('send_com'):
-    url += '/devices/' + args.dev_id + '/commands/' + args.action
-    data = { 'command': args.action, 'parameter': args.param, 'commandType': args.type }
-    data = urlencode(data)
-    data = data.encode('ascii')
-    headers['Content-Type'] = 'application/json'
-else:
-    print("Command error: ", args.command)
-    sys.exit(1)
-# print(url)
+    def do_get(self, url, data, headers={}):
+        req = Request(url, data, headers)
+        req.add_header('Authorization', self.token)
+        res = urlopen(req)
+        return res.read().decode()
 
-req = Request(url, data, headers)
-req.add_header('Authorization', token)
-try:
-    res = urlopen(req)
-except HTTPError as e:
-    print('Error code: ', e.getcode())
-    sys.exit(1)
-except URLError as e:
-    print('Reason: ', e.reason)
-    sys.exit(1)
+    def get_devices(self):
+        url = self.get_url('devices')
+        body = self.do_get(url, None)
+        dict = json.loads(body)
+        return dict['body']['deviceList']
+       
+    def get_status(self, id):
+        url = self.get_url('devices', id, 'status')
+        body = self.do_get(url, None)
+        dict = json.loads(body)
+        return dict['body']
 
-body = res.read().decode()
-print(body)
+    def send_command(self, id, command, param, type):
+        url = self.get_url('devices', id, command)
+        data = { 'command': args.action, 'parameter': args.param, 'commandType': args.type }
+        data = urlencode(data)
+        data = data.encode('ascii')
+        headers = {'Content-Type' : 'application/json'}
+        body = self.do_get(url, data, headers)
+        dict = json.loads(body)
+        return dict
+
+if __name__ == '__main__':
+    argparser = ArgumentParser(description='SwitchBot device control.')
+    argparser.add_argument('command', type=str, help='get_devices|get_status|post_action')
+    argparser.add_argument('-d', '--dev_id',    type=str, dest='dev_id',  default='dev_id',  help='device id for reading status')
+    argparser.add_argument('-a', '--action',    type=str, dest='action',  default='turnOn',  help='command to send device')
+    argparser.add_argument('-p', '--parameter', type=str, dest='param',   default='default', help='parameter of command')
+    argparser.add_argument('-t', '--type',      type=str, dest='type',    default='command', help='type of command')
+    args = argparser.parse_args()
+
+    sb = SwitchBot()
+    if args.command.startswith('get_dev'):
+        dict = sb.get_devices()
+    elif args.command.startswith('get_sta'):
+        dict = sb.get_status(args.dev_id)
+    elif args.command.startswith('send_com'):
+        dict = sb.send_command(args.dev_id, args.action, args.param, args.type)
+    else:
+        print("Command error: ", args.command)
+        sys.exit(1)
+
+    print(json.dumps(dict))
