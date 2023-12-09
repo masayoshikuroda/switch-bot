7f8b9b8600de9e0459070e7bd8af2b5965a65500
masayoshikuroda <masayoshi@mba2020.lan>
2023-12-09 21:23:16 +0900

    modified:   meter_ble.py

--- meter_ble.py
+++ meter_ble.py
@@ -3,8 +3,8 @@ import json
 import asyncio
 from bleak import BleakScanner
 
-SERV_UUID = 'cba20d00-224d-11e6-9fb8-0002a5d5c51b'
-CHAR_UUID = '00000d00-0000-1000-8000-00805f9b34fb'
+SERV_UUID = '0000fd3d-0000-1000-8000-00805f9b34fb'
+CHAR_UUID = 2409
 
 device_types = {
     0x48 : 'SwitchBot Bot (WoHand)',
@@ -19,6 +19,11 @@ device_types = {
     0x54 : 'SwitchBot MeterTH (WoSensorTH)/Normal',
     0x4D : 'SwitchBot Mini (HubMini)/Add Mode',
     0x6D : 'SwitchBot Mini (HubMini)/Normal Mode',
+    0x64 : 'Unknown d',
+    0x6A : 'Unknwon j',
+    0x6D : 'Unknown m',
+    0x76 : 'Unknwon v',
+    0x77 : 'SwitchBot MeterOutdoor(WoIOSensor)/w',
 }
 
 meters = {}
@@ -31,23 +36,18 @@ def detection_callback(device, advertisement_data):
 #    print(advertisement_data.manufacturer_data, end=' ')
 #    print(advertisement_data.service_uuids,     end=' ')
 #    print(advertisement_data.service_data,      end=' ')
-    md = advertisement_data.manufacturer_data
-    uuids = advertisement_data.service_uuids
-    sd = advertisement_data.service_data
-    if SERV_UUID in uuids:
-        cd = sd.get(CHAR_UUID)
-        type = device_types[cd[0]]
-#        print(type, end=' ')
-        if type.startswith('SwitchBot MeterTH '):
-            deviceId   = device.address.replace(':', '')
+#    print()
+    if SERV_UUID in advertisement_data.service_data:
+        sd = advertisement_data.service_data.get(SERV_UUID)
+        md = advertisement_data.manufacturer_data.get(CHAR_UUID)
+        type = device_types[sd[0]]
+        if type.startswith('SwitchBot Meter'):
+            deviceId = md[0:6].hex(':')
             deviceType = 'Meter'
-            temp = (cd[4] & 0x7f) + (cd[3] & 0x0f)/10
-            humi = (cd[5] & 0x7f)
+            temp = (md[9] & 0x7f) + (md[8] & 0x0f)/10
+            humi = (md[10] & 0x7f)
             meter = { 'deviceId' : deviceId, 'deviceType' : deviceType, 'temperature' : temp, 'humidity' : humi}
             meters[deviceId] = meter
-#            print('Temperature', temp, 'degC', end=' ')
-#            print('Humidity',    humi, '%',    end=' ')
-#    print()
 
 async def main():
     scanner = BleakScanner()
